ba2615d2f76f379bd6d101f2e053d7318fb9db64
projectvsbec-ops <projectvsbec@gmail.com>
2026-05-13 21:40:45 +0530

    feat: Add Two Sum

--- Array/Two Sum/python/solution.py
+++ Array/Two Sum/python/solution.py
@@ -1,7 +1,7 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return i,j
-        return -1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    return i,j
+        return -1
